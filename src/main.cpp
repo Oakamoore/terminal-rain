d5300c042294f331337bffc455c031d00bf95668
Oakamoore <emmanuelogie550@yahoo.com>
2024-07-13 12:37:36 +0100

    Add quit logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,35 @@
-#include "drop.h"
 #include "rain.h"
 #include "terminal.h"
-#include <ftxui/screen/screen.hpp>
-#include <iostream>
+#include <ftxui/screen/terminal.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/loop.hpp>
 #include <thread>
 #include <chrono>
 
 int main()
 {
-	auto screen {ftxui::Screen::Create(ftxui::Dimension::Full(), ftxui::Dimension::Full())};
+	using namespace std::chrono_literals;
+	
+	// Uses the primary screen buffer, ensuring that terminal resizes are responsive
+	auto screen {ftxui::ScreenInteractive::FixedSize(ftxui::Terminal::Size().dimx, ftxui::Terminal::Size().dimy)};
+
+	const auto frameDuration {10ms};
 
 	Rain rain {};
-	
+
 	// Sets the initial number of drops
 	rain.resize(screen.dimx()); 
 
-	while (true)
+	// A blank component used to catch input events
+	ftxui::Component component {};
+
+	Terminal::setupInput(screen, component);
+	
+	// Registers and responds to relevant events
+	ftxui::Loop input {&screen, component};
+
+	while (!input.HasQuitted())
 	{
 		if (Terminal::hasResized(screen))
 		{
@@ -29,8 +43,10 @@ int main()
 
 		Terminal::printScreen(screen);
 
-		using namespace std::chrono_literals;
-		std::this_thread::sleep_for(1ms);
+		std::this_thread::sleep_for(frameDuration);
+
+		// Process any input events 
+		input.RunOnce();
 	}
 
 	return 0;
--- src/terminal.cpp
+++ src/terminal.cpp
@@ -1,11 +1,29 @@
 #include "terminal.h"
 #include <ftxui/screen/terminal.hpp>
-#include <ftxui/screen/screen.hpp>
+#include <ftxui/component/event.hpp>
 #include <string>
 #include <iostream>
 
 namespace Terminal
 {
+	void setupInput(ftxui::ScreenInteractive& screen, ftxui::Component& component)
+	{
+		component = ftxui::Renderer([] { return ftxui::text(""); });
+
+		// Decorates the component to catch a 'quit' event
+		component |= ftxui::CatchEvent([&] (ftxui::Event event)
+		{
+			if (event == ftxui::Event::Character('q') || 
+				event == ftxui::Event::Escape)
+			{
+				screen.ExitLoopClosure()();
+				return true;
+			}
+
+			return false;
+		});
+	}	
+
 	bool hasResized(const ftxui::Screen& screen)
 	{
 		if (screen.dimx() != ftxui::Terminal::Size().dimx || 
@@ -21,14 +39,16 @@ namespace Terminal
 	{
 		// Updates the screen to match the terminal's current dimensions
 		screen = ftxui::Screen::Create(ftxui::Dimension::Full(), ftxui::Dimension::Full());
+
+		screen.Clear();
 	}
 	
 	void printScreen(ftxui::Screen& screen)
 	{
 		static std::string resetPosition {};
 
-		// Resets the cursor position, preventing the terminal from scrolling
-		std::cout << resetPosition << screen.ToString() << std::flush;
+		// Hides the cursor, and resets its position, preventing the terminal from scrolling
+		std::cout << "\033[?25l" << resetPosition << screen.ToString() << std::flush;
 		resetPosition = screen.ResetPosition();
 
 		screen.Clear();
