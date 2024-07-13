9630a84fb63bca209d0027e61c9289e09068c21b
Oakamoore <emmanuelogie550@yahoo.com>
2024-07-13 20:15:09 +0100

    Remove unnecessary comments

--- src/rain.cpp
+++ src/rain.cpp
@@ -2,8 +2,6 @@
 
 void Rain::resize(int screenWidth)
 {
-	// Used to determine the number of drops needed 
-	// based on the current width of the screen 
 	static constexpr float dropCoefficient {0.75f};
 	
 	int numDrops {static_cast<int>(screenWidth * dropCoefficient)};
@@ -11,15 +9,14 @@ void Rain::resize(int screenWidth)
 	// Prevents unnecessary reallocations
 	m_drops.reserve(numDrops);
 
-	// The width of the screen has increased,
-	// or 'm_drops' is currently empty 
+	// The width of the screen has increased, or 'm_drops' is currently empty 
 	if (m_drops.size() < numDrops)
 	{
 		// Adds as many drops as are needed
 		for (auto i {m_drops.size()}; i != numDrops; ++i)
 			m_drops.emplace_back(std::make_unique<Drop>()); 
 	}
-	else if (m_drops.size() > numDrops) // Screen width has decreased
+	else if (m_drops.size() > numDrops) 
 	{
 		// Destroys any drops that are out of bounds
 		std::erase_if(m_drops, [screenWidth] (const auto& drop)
